c1b9c5fadb9abcf0361ecd4745b554723d11c4ff
htavli <htavli@proton.me>
2024-07-13 02:42:20 +0300

    Solution 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function (string, repeatCount) {
+  if (repeatCount < 0) {
+    return "ERROR";
+  }
+  let res = "";
+  for (let i = 0; i < repeatCount; i++) {
+    res += string;
+  }
+  return res;
 };
 
 // Do not edit below this line
